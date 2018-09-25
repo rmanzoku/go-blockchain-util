69a3b71d4ad03bb8edc9e03a10e161c0f776f7f0
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-09-25 06:25:15 +0000

    Loom util

---
+++ loom-util/account.go
@@ -0,0 +1,51 @@
+package loomutil
+
+import (
+	"encoding/base64"
+
+	loom "github.com/loomnetwork/go-loom"
+	"github.com/loomnetwork/go-loom/auth"
+	"golang.org/x/crypto/ed25519"
+)
+
+// Account struct
+type Account struct {
+	PrivateKey       ed25519.PrivateKey
+	PrivateKeyBase64 string
+	PublicKey        ed25519.PublicKey
+	PublicKeyBase64  string
+	ChainID          string
+	Address          string
+	Signer           auth.Ed25519Signer
+}
+
+// NewAccount return *Account from PrivateKey
+func NewAccount(chainID string, pri ed25519.PrivateKey) (*Account, error) {
+	ret := &Account{}
+	ret.PrivateKey = pri
+	ret.PublicKey = ret.PrivateKey.Public().(ed25519.PublicKey)
+	ret.PrivateKeyBase64 = base64.StdEncoding.EncodeToString(ret.PrivateKey)
+	ret.PublicKeyBase64 = base64.StdEncoding.EncodeToString(ret.PublicKey)
+	ret.Address = loom.LocalAddressFromPublicKey(ret.PublicKey).String()
+	ret.Signer = *auth.NewEd25519Signer(ret.PrivateKey)
+
+	return ret, nil
+}
+
+// NewAccountFromPrivateKeyBase64 return *Account from PrivateKeyBase64
+func NewAccountFromPrivateKeyBase64(chainID string, priBase64 string) (*Account, error) {
+	pri, err := base64.StdEncoding.DecodeString(priBase64)
+	if err != nil {
+		return nil, err
+	}
+	return NewAccount(chainID, pri)
+}
+
+// GenerateAccount generate new PrivateKey and return *Account
+func GenerateAccount(chainID string) (*Account, error) {
+	_, pri, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		return nil, err
+	}
+	return NewAccount(chainID, pri)
+}
